6d04aa3995daac9be24f70462261f2e0c9014090
CBalthazar <balthclement@gmail.com>
2025-06-26 13:32:53 +0200

    contain fatah room5

--- rooms/room1/script.js
+++ rooms/room1/script.js
@@ -1,13 +1,72 @@
-console.info('Room 1 script loaded');
-
-const allButons = document.querySelectorAll('button');
-allButons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    const userInput = event.target.dataset.id || event.target.textContent;
-    const answerEvent = new CustomEvent('answer-submitted', {
-      detail: { value: userInput },
-    });
-    document.dispatchEvent(answerEvent);
-    /*     window.submitAnswer(userInput); */
+window.validateAnswer = function (answer) {
+  return new Promise((resolve, reject) => {
+    if (answer.toLowerCase() === "alohomora") {
+      resolve();
+    } else {
+      reject("Nope! Hint: This spell unlocks doors.");
+    }
   });
+};
+
+const passwordInput = document.querySelector("#password");
+const submitButton = document.querySelector("#submit");
+
+submitButton.addEventListener("click", checkAnswer);
+submitButton.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    checkAnswer(e);
+  }
 });
+
+function PlaySentence(text) {
+  const words = text.toLowerCase().split(" ");
+  const morse = words.map(PlayWord);
+  console.log(morse);
+}
+function PlayWord(word) {
+  word.split("").map(asciiToMorse);
+}
+
+function asciiToMorse(letter) {
+  return MORSE_MAP[letter];
+}
+
+function checkAnswer(event) {
+  event.preventDefault();
+  const originalText = event.target.value;
+  const userText = originalText.replace(/\s+/g, "");
+
+  const answerEvent = new CustomEvent("answer-submitted", {
+    detail: { value: userText },
+  });
+  document.dispatchEvent(answerEvent);
+}
+
+const MORSE_MAP = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+};
--- rooms/room2/script.js
+++
@@ -1,9 +0,0 @@
-window.validateAnswer = function (answer) {
-  return new Promise((resolve, reject) => {
-    if (answer.toLowerCase() === 'alohomora') {
-      resolve();
-    } else {
-      reject('Nope! Hint: This spell unlocks doors.');
-    }
-  });
-};
