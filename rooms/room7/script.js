3926a9bad7fa83f9d089b0239d83393a112660ad
Stagiaire 7 Pau <stagiaire7-pau@afec.fr>
2025-06-26 11:25:58 +0200

    fire test

--- rooms/room7/script.js
+++ rooms/room7/script.js
@@ -15,6 +15,29 @@ const h1 = document.getElementById("enigma-number")
 const clickMe = document.getElementById("click-me")
 h1.style.display = "none"
 http.play()
+setTimeout(() => {
+  console.log("WHAT ARE YOU EVEN DOING ?")
+},5000)
+
+setTimeout(()=> {
+  console.log("YOU HAVE NO POWER HERE")
+}, 7000)
+
+setTimeout(() => {
+  console.log("I'LL SHOOT YOU")
+}, 30000)
+setTimeout(() => {
+  console.log("RIGHT")
+}, 30500)
+setTimeout(() => {
+  console.log("BETWEEN")
+}, 31000)
+setTimeout(() => {
+  console.log("THE")
+}, 31500)
+setTimeout(() => {
+  console.log("EYES")
+}, 32000)
 
 clickMe.addEventListener("click", () => {
   qrcode.generate("3430c2b033302735302e36224e203839c2b030332731362e362257")
