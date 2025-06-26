d778728dd857b8ec9bd83a51701d25957275d916
Stagiaire 7 Pau <stagiaire7-pau@afec.fr>
2025-06-26 12:05:43 +0200

    add hints

--- rooms/room7/script.js
+++ rooms/room7/script.js
@@ -9,39 +9,52 @@ window.validateAnswer = function (answer) {
     }
   });
 };
-
+const hintTime = 15000
 const http = document.getElementById("http")
 const h1 = document.getElementById("enigma-number")
-const clickMe = document.getElementById("click-me")
+const clickMe = document.getElementById("636C69636B6D65")
+const helpEl = document.getElementById("help")
 h1.style.display = "none"
 http.play()
+
 setTimeout(() => {
-  console.log("WHAT ARE YOU EVEN DOING ?")
+  helpEl.style.display = "block"
+}, 300000)
+
+helpEl.addEventListener("click", giveHint)
+
+setTimeout(() => {
+  console.log("DO YOU EVEN KNOW WHAT YOU'RE DOING ?")
 },5000)
 
 setTimeout(()=> {
   console.log("YOU HAVE NO POWER HERE")
 }, 7000)
 
-setTimeout(() => {
-  console.log("I'LL SHOOT YOU")
-}, 30000)
-setTimeout(() => {
-  console.log("RIGHT")
-}, 30500)
-setTimeout(() => {
-  console.log("BETWEEN")
-}, 31000)
-setTimeout(() => {
-  console.log("THE")
-}, 31500)
-setTimeout(() => {
-  console.log("EYES")
-}, 32000)
+function giveHint() {
+  setTimeout(() => {
+    console.log("I'LL SHOOT YOU")
+  }, hintTime)
+  setTimeout(() => {
+    console.log("RIGHT")
+  }, hintTime + 500)
+  setTimeout(() => {
+    console.log("BETWEEN")
+  }, hintTime + 1000)
+  setTimeout(() => {
+    console.log("THE")
+  }, hintTime + 1500)
+  setTimeout(() => {
+    console.log("EYES")
+  }, hintTime + 2000)
+}
 
 clickMe.addEventListener("click", () => {
   qrcode.generate("3430c2b033302735302e36224e203839c2b030332731362e362257")
   setTimeout(()=> {
-    console.info("You found me, you know hexactly what to do")
+    console.info("SO YOU FOUND ME ?")
   }, 5000)
+  setTimeout(()=> {
+    console.info("SEEMS LIKE YOU KNOW YOU HEXACTLY WHAT YOU'RE DOING")
+  },5500)
 })
