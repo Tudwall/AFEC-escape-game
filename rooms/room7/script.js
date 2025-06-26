b931c2d67f9a7992f540656f5975ac955ca00389
Stagiaire 7 Pau <stagiaire7-pau@afec.fr>
2025-06-26 11:10:35 +0200

    add first step of enigma and style

--- rooms/room7/script.js
+++ rooms/room7/script.js
@@ -1,3 +1,5 @@
+import qrcode from "qrcode-terminal"
+
 window.validateAnswer = function (answer) {
   return new Promise((resolve, reject) => {
     if (answer.toLowerCase() === 'alohomora') {
@@ -8,10 +10,15 @@ window.validateAnswer = function (answer) {
   });
 };
 
-/* const boomBoom = document.getElementById("boom-boom");
-boomBoom.addEventListener("volumechange", () => {
-    boomBoom.volume = 100
-}) */
+const http = document.getElementById("http")
+const h1 = document.getElementById("enigma-number")
+const clickMe = document.getElementById("click-me")
+h1.style.display = "none"
+http.play()
 
-const boomBoom = new Audio("../../assets/sound/boomboomboomboom.mp3");
-boomBoom.play();
+clickMe.addEventListener("click", () => {
+  qrcode.generate("3430c2b033302735302e36224e203839c2b030332731362e362257")
+  setTimeout(()=> {
+    console.info("You found me, you know hexactly what to do")
+  }, 5000)
+})
